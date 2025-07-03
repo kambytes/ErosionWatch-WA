7297593de92e467302478e6179647ec44ac25867
kambytes <itskamleshkumar.s@gmail.com>
2025-07-04 03:46:57 +0800

    main content section added. Footer section added.

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -1,4 +1,4 @@
-/* Nav-bar toggle dropdown menu button*/
+/* Nav-bar dropdown menu button*/
 document.getElementById('learn-toggle').addEventListener('click', function () {
   const dropdown = this.closest('.dropdown');
   dropdown.classList.toggle('open');
