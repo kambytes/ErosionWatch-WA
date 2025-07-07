4e18c4f529a9afbe15f1aaa1bb43655eb7e7768a
kambytes <itskamleshkumar.s@gmail.com>
2025-07-07 22:22:50 +0800

    Used GeoPandas to reproject original shapefile (EPSG:7844) to WGS84 (EPSG:4326)

--- app/scripts/erosion_hotspots_script.py
+++ app/scripts/erosion_hotspots_script.py
@@ -8,6 +8,10 @@ def shapefile_to_geojson(input_file_path, output_file_path):
     # Open and read shapefile
     gdf = gpd.read_file(input_file_path)
 
+    # Shapefile with its original CRS (EPSG:7844)
+    # Reproject to EPSG:4326 (WGS84) for compatibility with Leaflet.js
+    gdf = gdf.to_crs(epsg=4326)
+
     # Write the shapefile into new .geojson file
     gdf.to_file(output_file_path, driver="GEOJSON")
 
@@ -15,4 +19,4 @@ def shapefile_to_geojson(input_file_path, output_file_path):
 shapefile_to_geojson(SHAPEFILE_FILE, GEOJSON_FILE)
 
 # Print statement for successful execution of file conversion
-print("The shapefile has been converted into GEOJSON file format.")
+print("The shapefile has been converted into GEOJSON file format. Reprojected using Leaflet.js coordinate system.")
