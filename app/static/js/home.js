9cfec7020bea4ccfb0a226089fc0af9df7ab18a1
kambytes <itskamleshkumar.s@gmail.com>
2025-07-19 04:17:36 +0800

    gallery image slideshow has been completed. HTML, CSS formatting and JS functionality all added.

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -4,3 +4,39 @@ document.getElementById('learn-toggle').addEventListener('click', function () {
   dropdown.classList.toggle('open');
 });
 
+/* This is the function which provides functionality to gallery slideshow */
+let index = 0;
+const slides = document.querySelectorAll(".gallery");
+const total = slides.length;
+galleryMoveSlide(index);
+
+function galleryMoveSlide(currentIndex) {
+  slides.forEach(slide => slide.style.display = 'none');
+
+  if (currentIndex >= total) {
+    index = 0;
+  } else if (currentIndex < 0) {
+    index = total - 1;
+  } else {
+    index = currentIndex;
+  }
+
+  slides[index].style.display = "block";
+
+  // Highlight the correct dot
+  const dots = document.querySelectorAll(".dot");
+  dots.forEach(dot => dot.classList.remove("active"));
+  dots[index].classList.add("active");
+}
+
+function previousSlide() {
+  galleryMoveSlide(index - 1); // move backward
+}
+
+function nextSlide() {
+  galleryMoveSlide(index + 1); // move forward
+}
+
+function goToSlide(i) {
+  galleryMoveSlide(i);
+}
